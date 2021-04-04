18913136755ed2f84bcb4eeb38e0376aa6be0f01
unknown <alexandre2010nogueira@hotmail.com>
2021-04-04 18:47:20 -0300

    Consumo Tema - Get and Post

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -2,6 +2,8 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -10,13 +12,37 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  tema: Tema = new Tema()
+  listTema: Tema[]
+
+  constructor(private router: Router,
+    private temaService: TemaService) { }
 
   ngOnInit(){
-    /*if(environment.token == ''){
+    if(environment.token == ''){
       alert('Sua sessão expirou, faça login novamente!')
-      this.router.navigate(['/entrar'])
-    }*/
+      this.router.navigate(['/entrar']) }
+
+      this.findAllTema()
+
   }
+
+
+  findAllTema(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=>{
+      this.listTema = resp
+    })
+  }
+
+  cadastrarTema(){
+      this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+        this.tema = resp
+        alert('Tema cadastrado!')
+        this.findAllTema()
+        this.tema = new Tema()
+      })
+  }
+
+
 
 }
